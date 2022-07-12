fab71b0e57b28c3a2958f4b860f8f3a5e04759fb
Clopas <16157207+soroush01110011@users.noreply.github.com>
2022-07-12 18:38:37 +0430

    minor change

--- credentials.py
+++ credentials.py
@@ -1,12 +1,12 @@
 # ##################### API KEYS ###########################################
-#account_id_3commas = form.account_id_3commas_signup.data
-#api_key_3commas = form.api_key_3commas_signup.data
-#api_secret_3commas = form.api_secret_3commas_signup.data
-#api_key_ftx = form.api_key_ftx_signup.data
-#api_secret_ftx = form.api_secret_ftx_signup.data
+account_id_3commas = form.account_id_3commas_signup.data
+api_key_3commas = form.api_key_3commas_signup.data
+api_secret_3commas = form.api_secret_3commas_signup.data
+api_key_ftx = form.api_key_ftx_signup.data
+api_secret_ftx = form.api_secret_ftx_signup.data
 
-api_key_3commas = ''
-api_secret_3commas = ''
-account_id_3commas = ''
-api_key_ftx = ''
-api_secret_ftx = ''
+# api_key_3commas = ''
+# api_secret_3commas = ''
+# account_id_3commas = ''
+# api_key_ftx = ''
+# api_secret_ftx = ''
